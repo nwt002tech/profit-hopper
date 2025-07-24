609f06da59adb1cd17f4159db843548291cd5d0f
nwt002tech <56565545+nwt002tech@users.noreply.github.com>
2025-07-24 14:41:23 -0500

    Add files via upload

--- profit_hopper_modern.py
+++ profit_hopper_modern.py
@@ -53,7 +53,6 @@ def get_recommended_games(game_df, session_bankroll, max_bet):
                 st.session_state["session_log"] = []
             if "session_log" in st.session_state and st.session_state["session_log"]:
                 with tab3:
-        with tab3:
             if "session_log" in st.session_state:
                 net = total_out - total_in
             else:
